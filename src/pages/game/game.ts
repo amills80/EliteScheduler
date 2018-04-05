0b0a2f66325cb44c538fd97acb932c5d373f9166
Alan Mills <aelonrench@yahoo.com>
2018-04-05 18:15:50 -0400

    Implement LoDash with filtering of Json input of teams within tourney.  Need to QAQC data acquisition.

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,24 +1,26 @@
+import { EliteApi } from './../../providers/elite-api/elite-api';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the GamePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @Component({
   selector: 'page-game',
   templateUrl: 'game.html',
 })
 export class GamePage {
+public game: any = {};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public eliteApi: EliteApi) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad GamePage');
+    this.game = this.navParams.data;
   }
 
+  teamTapped() {
+    let tourneyData = this.eliteApi.getCurrentTourney();
+  }
 }
