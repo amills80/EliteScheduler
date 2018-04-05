9bcd2cdf1a76af92212f285aa0a6c538d94c26dd
Alan Mills <aelonrench@yahoo.com>
2018-04-05 06:21:59 -0400

    use RxJS to fetch tournament data and team data for teams page.

--- src/providers/elite-api/elite-api.ts
+++ src/providers/elite-api/elite-api.ts
@@ -1,20 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 
 
 @Injectable()
 export class EliteApi {
-
   private baseUrl='https://elite-sched-app-ion2.firebaseio.com/';
-
-  constructor(public http: Http) {
-  }
+  private currentTourney: any = {};
+  constructor(public http: Http) {  }
 
   getTournaments(){
     return new Promise(resolve => {
       this.http.get(`${this.baseUrl}/tournaments.json`).subscribe(res => resolve(res.json()));
     });
+  }
+
+  getTournamentData(tourneyId) : Observable<any>{
+    return this.http.get(`${this.baseUrl}/tournaments-data/${tourneyId}.json`)
+    .map(response => {
+      this.currentTourney = response.json();
+    return this.currentTourney;
+    })
   }
 }
