45de128cd80c835c1f8e648aa349a8937b51ffc3
Alan Mills <aelonrench@yahoo.com>
2018-04-06 11:27:38 -0400

    Add sample favorite to MyFavorites page with connectivity.

--- src/pages/my-teams/my-teams.ts
+++ src/pages/my-teams/my-teams.ts
@@ -1,6 +1,8 @@
+import { TeamHomePage } from './../team-home/team-home';
+import { EliteApi } from './../../providers/elite-api/elite-api';
 import { TournamentsPage } from './../tournaments/tournaments';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, NavParams, Loading, LoadingController } from "ionic-angular";
 
 @Component({
     selector: 'page-my-teams',
@@ -8,8 +10,31 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
 })
 
 export class MyTeamsPage{
-    constructor(private nav: NavController) {}
+
+    favorites = [
+        {
+            team:{ id: 6182, name: 'HC Elite 7th', coach: 'Michelotti' },
+            tournamentId: '89e13aa2-ba6d-4f55-9cc2-61eba6172c63', 
+            tournamentName: 'March Madness Tournament'
+        }
+    ];
+
+    constructor(
+        private nav: NavController,
+        private loadingController: LoadingController,
+        private eliteApi: EliteApi) {}
+        
     goToTournaments() {
         this.nav.push(TournamentsPage)
+    }
+
+    favoriteTapped($event, favorite){
+        let loader = this.loadingController.create({
+            content: "Getting data...",
+            dismissOnPageChange: true
+        });
+        loader.present();
+        this.eliteApi.getTournamentData(favorite.tournamentId)
+            .subscribe(t => this.nav.push(TeamHomePage, favorite.team));
     }
 }
