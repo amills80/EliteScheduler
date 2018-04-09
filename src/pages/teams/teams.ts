e3d4f052e46f9e69e8b665c76444ddde87a197fc
Alan Mills <aelonrench@yahoo.com>
2018-04-09 07:21:53 -0400

    Add seach bar function to teams listings.

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -13,6 +13,7 @@ export class TeamsPage {
   public teams = [ ];
   private allTeams: any;
   private allTeamDivisions: any;
+  public queryText: string
 
   constructor(
     public navCtrl: NavController, 
@@ -46,5 +47,20 @@ export class TeamsPage {
 
   itemTapped($event, team){
     this.navCtrl.push(TeamHomePage, team);
+  }
+
+  updateTeams() {
+    let queryTextLower = this.queryText.toLowerCase();
+    let filteredTeams = [];
+    _.forEach(this.allTeamDivisions, td => {
+      let teams = _.filter(td.divisionTeams, t => (<any>t).name.toLowerCase().includes(queryTextLower));
+      if (teams.length) {
+        filteredTeams.push({
+          divisionName: td.divisionName, 
+          divisionTeams: teams
+        });
+      }
+    });
+    this.teams = filteredTeams;
   }
 }
